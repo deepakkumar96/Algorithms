9c157f82cd18d8a307addf60565e88176d875b26
Deepak Kumar <deepakkumar24196@gmail.com>
2019-10-27 01:06:26 +0530

    largest bst in binary tree

--- Tree/largest_bst_subtree.cpp
+++ Tree/largest_bst_subtree.cpp
@@ -1,5 +1,11 @@
 
 /************************************ Question ***************************************
+
+Source	: InterviewBit
+Type  	: Tree
+Link	: https://www.interviewbit.com/problems/largest-bst-subtree
+Question Description:
+
 Largest BST Subtree
 Given a binary tree. Find the size of largest subtree which is a Binary Search Tree (BST), where largest means subtree with the largest number of nodes in it. Note: A subtree must include all of its descendants. For Example
 Input 1:
@@ -26,9 +32,12 @@ Output 2:
      7
      Explanation 2:
         Given binary tree itself is BST.
-*/
 
-/************************************* Solution *******************************************/
+
+
+
+************************************* Solution ***************************************/
+
 
 /**
  * Definition for binary tree
@@ -39,6 +48,37 @@ Output 2:
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-int Solution::solve(TreeNode* A) {
-	
+
+int findSize(TreeNode* root){
+    if(not root) {
+        return 0;
+    }
+    return findSize(root->left) + findSize(root->right) + 1;
+}
+
+bool isBst(TreeNode* root, int minVal, int maxVal){
+    if(not root)  {
+        return true;
+    }
+    if(root->val<minVal or root->val>maxVal) {
+        return false;
+    }
+    return isBst(root->left, minVal, root->val) and isBst(root->right, root->val, maxVal);
+}
+
+
+int findLargestBst(TreeNode* root){
+    if(not root)  {
+        return 0;
+    }
+    if(isBst(root, INT_MIN, INT_MAX)) {
+        return findSize(root);
+    }
+    return max(findLargestBst(root->left), findLargestBst(root->right));
+}
+
+int Solution::solve(TreeNode *root) {
+	/*auto maxSubTree = findMaxSubTree(root, INT_MIN, INT_MAX);
+	return maxSubTree.second;*/	
+	return findLargestBst(root);
 }
