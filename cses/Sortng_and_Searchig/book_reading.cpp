9613e7e081ced1924f742e9ca1b8e84099b366c8
deepak.sonava <deepak.sonava@dailyhunt.in>
2022-05-17 23:48:08 +0530

    cses book reading

--- cses/Sortng_and_Searchig/book_reading.cpp
+++ cses/Sortng_and_Searchig/book_reading.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#inclide <vector>
+#include <vector>
 
 using namespace std;
 
 int main() {
-	int n, mx, x;
+	int n, mx = 0, x;
 	long long int sum = 0;
+	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> x;
 		sum += x;
