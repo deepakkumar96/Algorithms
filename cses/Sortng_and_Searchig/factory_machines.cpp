ec2cdbad82b99aa2680e6eb311b90e16df6707cd
deepak.sonava <deepak.sonava@dailyhunt.in>
2022-06-28 11:47:12 +0530

    cses - 28 June 2022

---
+++ cses/Sortng_and_Searchig/factory_machines.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+bool isPossibleToMake(const vector<long long int> &ms, long long int mid, long long int productsReq) {
+	long long totalProducts = 0;
+	for (long long int v : ms) {
+		totalProducts += mid / v;
+	}
+	return totalProducts >= productsReq;
+}
+
+long long minTimeReqToMake(const vector<long long int> &ms, long long int sml, long long int t) {
+	long long maxTime = sml * t;
+	long long minTime = 1;
+
+	while (minTime < maxTime) {
+		long long mid = minTime + ((maxTime - minTime) / 2);
+		//cout << mid << " " << minTime << " " << maxTime << endl;
+		if (isPossibleToMake(ms, mid, t)) {
+			maxTime = mid;
+		} else {
+			minTime = mid + 1;
+		}
+	}
+	return maxTime;
+}
+
+int main () {
+	long long int n, t;
+	cin >> n >> t;
+	vector<long long int> ms(n, 0);
+	long long int sml = INT_MAX;
+	for (long long int i = 0; i < n; i++) {
+		cin >> ms[i];
+		sml = min(sml, ms[i]);
+	}
+	cout << minTimeReqToMake(ms, sml, t);
+	return 0;
+}
